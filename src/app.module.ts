5ffda1cc3579fc4c22fb7eddb3e94e655699d425
Enuupe <enuthiw05@gmail.com>
2025-01-31 15:11:43 +0530

    Database connection string commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://projectCicaeda:PC2025@projectcicaeda.quwjx.mongodb.net/?retryWrites=true&w=majority&appName=ProjectCicaeda') //connection string from mongoDB atlas
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
