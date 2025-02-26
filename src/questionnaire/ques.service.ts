e5eb0797a549612e54012eb5ee4458dac409add6
Enuupe <enuthiw05@gmail.com>
2025-02-26 23:55:35 +0530

    loading the questionnaire

--- src/questionnaire/ques.service.ts
+++ src/questionnaire/ques.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { QuestionnaireResult } from "src/schemas/ques.schema";
@@ -6,7 +6,7 @@ import * as fs from "fs";
 import * as path from "path";
  
 @Injectable()
-export class QuestionnaireService{
+export class QuestionnaireService implements OnModuleInit{
 
     private markingSystem: Record<string, Record<string, number>> = {};
 
