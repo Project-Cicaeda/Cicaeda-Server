e9bf3bdb42f24e62c057753311c2402996b02e43
Enuupe <enuthiw05@gmail.com>
2025-02-26 22:21:21 +0530

    loding questions from a different file

--- src/questionnaire/ques.service.ts
+++ src/questionnaire/ques.service.ts
@@ -2,39 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { QuestionnaireResult } from "src/schemas/ques.schema";
-
+import * as fs from "fs";
+import * as path from "path";
+ 
 @Injectable()
 export class QuestionnaireService{
-    constructor(@InjectModel(QuestionnaireResult.name) private readonly resultModel:Model<QuestionnaireResult>){}
 
-    private markingSystem = {
-        //medical conditions
-        question1: {Male: 0, Female: 1}, //gender
-        question2: {Yes: 1, No: 0}, //anemia
-        question3: {Yes: 1, No: 0}, //high blood pressure
-        question4: {Yes: 1, No: 0}, //diabetes
-        question5: {Yes: 0, No: 0}, //Cardio vascular disease
-
-        //medical history
-        question6: {Yes: 1, No: 0}, //heartattacks
-        question7: {Yes: 1, No: 0}, //congestive heart failure
-        question8: {Yes: 0, No: 0}, //fam. history of heart disease
-
-        //kidney health questions
-        question9: {Yes: 1, No: 0}, // protienuria 
-        question10: {Yes: 0, No: 0}, //foamy or bubbly urine
-        question11: {Yes: 0, No: 0}, //frequant urine passing
-        question12: {Yes: 0, No: 0}, //less urine passing
-        question13: {Yes: 0, No: 0}, //
-
-        //general health
-        question14: {Yes: 1, No: 0}, //ankle edema
-        question15: {Yes: 0, No: 0}, //low energy
-        question16: {Yes: 0, No: 0}, //brain fog
-        question17: {Yes: 0, No: 0}, //unexplained weight loss
-        question18: {Yes: 0, No: 0},
-        question19: {Yes: 0, No: 0},
-    };
+    private markingSystem: Record<string, Record<string, number>> = {};
 
     private ageMarkingSystem = [
         {min: 50, max: 59, points:2},
@@ -43,6 +17,18 @@ export class QuestionnaireService{
     ];
 
     readonly nonScorableFields = ["fName", "lName", "city", "address" ];
+
+    constructor(@InjectModel(QuestionnaireResult.name) private readonly resultModel:Model<QuestionnaireResult>){}
+
+    async onModuleInit(){
+        this.loadQuestions();
+    }
+
+    private loadQuestions(){
+        const filepath = path.join(__dirname, "../data/questions.json");
+        const filecontent = fs.readFileSync(filepath, "utf-8");
+        this.markingSystem = JSON.parse(filecontent);
+    }
 
     async calculation(responses: {key: string; value: string}[]){
         let total = 0;
@@ -53,7 +39,7 @@ export class QuestionnaireService{
         responses.forEach(({key, value}) => {
             
             //store string values seperately
-            if(["fName", "lName", "city", "address" ].includes(key)){
+            if(this.nonScorableFields.includes(key)){
                 userDetails[key] = value;
             }
 
