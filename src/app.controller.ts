62e4c49be40455f80b6c596bac2f19087974774d
Harindu-test <75421581+harindukalhara@users.noreply.github.com>
2025-03-03 19:30:09 +0530

    Adding Authentication guard to protect API routes
     * Restricting access to endpoints for only authenticated users.
     * User ID is passed in the token payload for validation
     * Error messages were added for invalid tokens
     * Authentication errors are logged for easier debugging

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("test")
-  getHello(): string {
-    console.log("SSS")
-    return this.appService.getHello();
+  @Get()
+  someProtectedRoute(@Req() req) {
+    return { message: 'Accessed Resource', userId: req.userId };  
+
   }
 }
