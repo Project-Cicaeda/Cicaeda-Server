d6a1a036c7a9ef01b4ff1b2ec22f0a487a54cc8c
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2025-03-06 19:59:11 +0530

    Model Loaded Using ONNX

--- src/tsf/tsf.service.ts
+++ src/tsf/tsf.service.ts
@@ -20,6 +20,25 @@ export class TsfService implements OnModuleInit{
       }
 
 
-
+    async predict(lastSequence:number[][]): Promise<number[]>{
+        if (!this.session) {
+            throw new Error("ONNX Model not loaded.");
+          }
+      
+          const forecasting = [];
+      
+          for (let i = 0; i < 12; i++) {
+            const inputTensor = new ort.Tensor('float32', new Float32Array(lastSequence.flat()), [1, 12, lastSequence[0].length]);
+      
+            const results = await this.session.run({ "input_1" : inputTensor });
+            console.log(results.dense.cpuData[0])
+            const prediction = results.dense.cpuData[0];
+      
+            forecasting.push(prediction);
+            lastSequence.shift();
+            lastSequence.push([prediction, ...lastSequence[lastSequence.length - 1].slice(1)]);
+          }
+          return forecasting
+    }
 
 }
