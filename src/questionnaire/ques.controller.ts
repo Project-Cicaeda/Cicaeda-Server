8223f9c4415b425252983bc52a01ac51c114b057
Enuupe <enuthiw05@gmail.com>
2025-03-11 11:04:43 +0530

    questionnaire controller update

--- src/questionnaire/ques.controller.ts
+++ src/questionnaire/ques.controller.ts
@@ -3,15 +3,17 @@ import { QuestionnaireService } from "./ques.service";
 import { QuestionnaireDto } from "src/dtos/questionnaire.dto";
 import { Request } from "express";
 
+//questionnaire controller
 @Controller('questionnaire')
 export class QuestionnaireController{
     constructor(private readonly questionnaireService: QuestionnaireService){}
 
+    //post request for submitting the answers
     @Post("submit")
     async submitQues(
         @Req() req: Request,
         @Body() {responses}: {responses: {key: string; value: string}[]}){
-            const email = req.user['email'];
+           // const email = req.user['email'];
         }
     }
 }
