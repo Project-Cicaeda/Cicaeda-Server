8382e84b518d4cbc8fa07d437af7f93ae4bd6eea
Enuupe <enuthiw05@gmail.com>
2025-03-11 11:15:17 +0530

    result controller update

--- src/result/result.controller.ts
+++ src/result/result.controller.ts
@@ -5,18 +5,21 @@ import { ResultService } from "./result.service";
 export class resultController{
     constructor(private readonly resultService: ResultService){}
 
+    //saving the scores according to user
     @Post('save')
     async saveResult(@Req() req, @Body() body){
 
+        //checking if the user exists in the database
         if(!req.User || !req.User.userID){
             throw new Error("user ID not found in payload");
         }
 
         const userID = req.User.userID;
         const {totalScore} = body;
-        return this.resultService.saveResult(userID, totalScore);
+        return this.resultService.saveResult(userID, totalScore); //save result 
     }
 
+    //getting the score history from the database 
     @Get('history')
     async getHistory(@Req() req){
         const userID = req.User.userID;
